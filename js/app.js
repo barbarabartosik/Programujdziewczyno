1e793a65b8096be4892bb9677856dd3f66c9a2db
barbara <barbara.bartosik@gmail.com>
2017-10-21 16:49:21 +0200

    zadanie 2 i 3

--- js/app.js
+++ js/app.js
@@ -9,7 +9,38 @@ document.addEventListener('DOMContentLoaded', function() {
         dropdownList.style.display = 'block';
        
     });
+    
     menuElement.addEventListener('mouseout', function(){
         dropdownList.style.display = 'none';
-            })
+    });
+    
+    // zadanie 2
+    var buttons = document.querySelectorAll('.btn.read-more');
+    
+    
+    
+    function showHide(){
+         var textArea = this.previousElementSibling;
+       
+       if(textArea.style.display === 'none' || textArea.style.display === '') {
+           textArea.style.display = 'block';
+    this.innerHTML='mniej<span class="glyphicon glyphicon-chevron-up"></span>';
+       } else {
+           textArea.style.display = 'none';
+           this.innerHTML='wiecej<span class="glyphicon glyphicon-chevron-down"></span>';
+       } 
+    }
+   
+    for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide); 
+        }
+
+//zadanie 3
+var navigation = document.querySelector('nav.navbar');
+window.addEventListener('scroll', function(){
+    navigation.style.borderBottom = '1px solid #7f7f7f';
 });
+});
+
+
+                    
